d9d71e991880dc166842096b9b7f0771e57b767a
unknown <amrm68853@gmail.com>
2026-02-01 15:20:37 +0200

    core/loader component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Loader } from './core/components/loader/loader';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , Loader , CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+   isLoading = true;
   protected title = 'zokhrof';
+
+ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 100000); // مؤقتًا
+  }
 }
